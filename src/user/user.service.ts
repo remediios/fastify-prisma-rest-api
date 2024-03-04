914694149b732496355a1595d7d05801351fcbdd
Miguel Remédios <miguelremediios@gmail.com>
2024-03-04 23:53:16 +0000

    feat: user service to deal with prisma requests and zod schema validation for the same function

---
+++ src/user/user.service.ts
@@ -0,0 +1,8 @@
+import prisma from '../utils/prisma';
+import { CreateUserInput } from './user.schema';
+
+export async function createUser(input: CreateUserInput) {
+  const user = await prisma.user.create({
+    data: input,
+  });
+}
