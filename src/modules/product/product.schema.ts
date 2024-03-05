ab8b50cf71990e5055fa8c3b1ef3b4947f26abcc
Miguel Remédios <miguelremediios@gmail.com>
2024-03-05 17:46:05 +0000

    feat: product file structure and schemas declaration

---
+++ src/modules/product/product.schema.ts
@@ -0,0 +1,38 @@
+import z from 'zod';
+import { buildJsonSchemas } from 'fastify-zod';
+
+//User input properties for product
+const productInput = {
+  title: z.string(),
+  content: z.string().optional(),
+  price: z.string(),
+};
+
+//Generated properties for product
+const productGenerated = {
+  id: z.number(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+};
+
+//Schema for creating a product
+const createProductSchema = z.object({
+  ...productInput,
+});
+
+//Response for a single product
+const productResponseSchema = z.object({
+  ...productInput,
+  ...productGenerated,
+});
+
+//Response for an array of products
+const productsResponseSchema = z.array(productResponseSchema);
+
+export type CreateProductInput = z.infer<typeof createProductSchema>;
+
+export const { schemas: productSchemas, $ref } = buildJsonSchemas({
+  createProductSchema,
+  productResponseSchema,
+  productsResponseSchema,
+});
